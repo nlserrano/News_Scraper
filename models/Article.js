441b1a4731465580214ee2b963138aa0e337c9dc
Nathan <nathanlserrano@gmail.com>
2019-02-26 15:49:36 -0800

    Updated scraping route

--- models/Article.js
+++ models/Article.js
@@ -1,37 +1,21 @@
-// Require mongoose
 var mongoose = require("mongoose");
-// Create Schema class
 var Schema = mongoose.Schema;
 
-// Create article schema
 var ArticleSchema = new Schema({
-  // title is a required string
-  title: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  // description is a required string
-  description: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  // boolean to flag articles as saved
-  saved: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
-  // This will save an array of comments' ObjectIds
-  comments:[{
-        type: Schema.ObjectId,
-        ref:'Comment'
-    }]
+	title: {
+		type: String,
+		required: true,
+	},
+	link: {
+		type: String,
+		required: true,
+	},
+	desc: String,
+	author: String,
+	comments: [{
+		type: Schema.Types.ObjectId,
+		ref: "Comment",
+	}],
 });
 
-// Create the Article model with the ArticleSchema
-var Article = mongoose.model("Article", ArticleSchema);
-
-// Export the model
-module.exports = Article;
+module.exports = mongoose.model("Article", ArticleSchema);
--- server.js
+++ server.js
@@ -1,58 +1,107 @@
-// Dependencies
 var express = require("express");
+var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var exphbs = require("express-handlebars");
-// Requiring Comment and Article models
-var Comment = require("./models/Comment.js");
-var Article = require("./models/Article.js");
-// Requiring routing controllers
-var htmlRouter = require("./controllers/html-routes.js");
-var articleRouter = require("./controllers/article-routes.js");
-// Scraping tools
 var request = require("request");
-var cheerio = require("cheerio");
-// Set mongoose to leverage built in JavaScript ES6 Promises
-mongoose.Promise = Promise;
+const cheerio = require("cheerio");
 
-// Initialize Express
-var port = process.env.PORT || 3000;
+const db = require("./models");
+
+const PORT = process.env.PORT || 3000;
 var app = express();
-
-// Use body parser with the app
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-// Initialize Handlebars
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Routing
-app.use("/", htmlRouter);
-app.use("/", articleRouter);
+// changed from "MONGODB_URI" to "URI"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlinestest";
 
-// Make public a static dir
-app.use(express.static("public"));
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
-// Database configuration with mongoose
-var URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/news-scraper'; 
-mongoose.connect(URI, { useNewUrlParser: true });
-mongoose.set('useCreateIndex', true);
-var db = mongoose.connection;
+// old code
+// mongoose.connect(MONGODB_URI, {
+// 	useMongoClient: true,
+// });
 
+// additional mongoose code
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// mongoose.set('useCreateIndex', true);
 
-// Show any mongoose errors
-db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
+app.get("/scrape", (req, res) => {
+	var url = "https://www.wired.com/most-popular/";
+	
+	return request(url, function(error, response, html) {
+		var $ = cheerio.load(html);
+		
+		var articles = [];
+		
+		$("li.archive-item-component").each((i, element) => {
+			let articleElement = $(element);
+			let article = {};
+			
+			let info = articleElement.children("div.archive-item-component__info").children("a.archive-item-component__link");
+			
+			article.link = "https://www.wired.com" + info.attr("href");
+			article.title = info.children("h2.archive-item-component__title").text();
+			article.desc = info.children("p.archive-item-component__desc").text();
+			article.author = articleElement.children("div.archive-item-component__info").children("div.archive-item-component__byline").children("span").children("div.byline-component--micro").children("span.byline-component__content[itemprop='author']").children("a.byline-component__link").text();
+			
+			articles.push(article);
+			//db.Article.update({title: article.title}, article, {upsert: true, setDefaultsOnInsert: true}).then(result => {});
+		});
+		
+		(function syncArticle(i) {
+			if (i < articles.length) {
+				//console.log(articles[i]);
+				db.Article.update({title: articles[i].title}, articles[i], {upsert: true, setDefaultsOnInsert: true}).then(result => {
+					return syncArticle(i + 1);
+				}).catch(err => {
+					console.log(err);
+				});
+			} else {
+				return res.json(true);
+			}
+		})(0);
+	});
+	
+	res.json(false);
 });
 
-// Once logged in to the db through mongoose, log a success message
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
+app.get("/", (req, res) => {
+	db.Article.find({}).then(dbArticles => {
+		res.render("index", {articles: dbArticles});
+	});
 });
 
-// Listen on port 3000
-app.listen(port, function() {
-  console.log("App running on port 3000!");
+app.post("/articles/:id", (req, res) => {
+	db.Comment.create(req.body).then(dbComment => {
+		return db.Article.findOneAndUpdate({_id: req.params.id}, {'$push': {comments: dbComment._id}}, { new: true });
+    }).then(dbArticle => {
+		res.json(dbArticle);
+	}).catch(function(err) {
+		res.json(err);
+	});
+});
+
+app.get("/articles/:id", (req, res) => {
+	db.Article.findOne({_id: req.params.id}).populate("comments").then((dbArticle) => {
+		res.json(dbArticle);
+	}).catch((err) => {
+		res.json(err);
+	});
+});
+
+app.delete("/comments/:id", (req, res) => {
+	db.Comment.findByIdAndRemove(req.params.id).then(result => {
+		res.json(true);
+	}).catch((err) => {
+		res.json(err);
+	});
+});
+
+app.listen(PORT, function() {
+  console.log("Server listening on port " + PORT);
 });
